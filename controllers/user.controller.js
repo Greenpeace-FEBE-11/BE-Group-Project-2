bab4aaa3032dc8e8ebaec61beff119b7a1c5f019
rosyamboro10 <rosyamborowati10@gmail.com>
2022-11-20 00:47:01 +0700

    login

---
+++ controllers/user.controller.js
@@ -0,0 +1,38 @@
+module.exports = {
+  getAllUser: (req, res) => {
+    
+  },
+
+  getUserByID: (req, res) => {
+
+  },
+
+  addUser: (req, res) => {
+    
+  },
+
+  deleteUserByID: (req, res) => {
+ 
+  },
+
+  updateUserByID: (req, res) => {
+
+  },
+}
+
+// exports.allAccess = (req, res) => {
+//   res.status(200).send("Public Content.");
+// };
+
+// exports.userBoard = (req, res) => {
+//   res.status(200).send("User Content.");
+// };
+
+// exports.adminBoard = (req, res) => {
+//   res.status(200).send("Admin Content.");
+// };
+
+// exports.moderatorBoard = (req, res) => {
+//   res.status(200).send("Moderator Content.");
+// };
+
---
+++ routes/auth.routes.js
@@ -0,0 +1,25 @@
+const { verifyRegister } = require("../middlewares");
+const controller = require("../controllers/auth.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post(
+    "/api/auth/register",
+    [
+      verifyRegister.checkDuplicateUsernameOrEmail,
+      verifyRegister.checkRolesExisted
+    ],
+    controller.register
+  );
+
+  app.post("/api/auth/login", controller.login);
+
+  // app.post("/api/auth/signout", controller.signout);
+};
