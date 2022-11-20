0fc6bdaaab74df323495402f240cf3f58a5e0f07
MOHAMAD FEBRIANTO MANANGI <91535685+GLynn6@users.noreply.github.com>
2022-11-20 22:56:05 +0800

    Dev (#1)
    * add erd
    
    * Update README.md
    
    * febrianto - user_page
    
    * febrianto - loginuserpage
    
    * fix packege.json
    
    * febrianto - fixed error
    
    * febrianto - update content reamde
    
    Co-authored-by: lisa0104 <59335342+lisa0104@users.noreply.github.com>

---
+++ routes/userpage.router.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const { authJwt } = require("../middlewares");
+const controller = require("../controllers/userpage.controller")
+
+
+
+module.exports = function(app) {
+    app.use(function(req, res, next) {
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, Content-Type, Accept"
+      );
+      next();
+    });
+  
+    app.get("/userpage", [authJwt.verifyToken], controller.getInformasi)
+    app.post("/userpage", [authJwt.verifyToken], controller.addInformasi)
+  
+  
+    
+  };
+
+
+
