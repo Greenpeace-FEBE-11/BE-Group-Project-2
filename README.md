8e8cc37e3cac0d54614907a480b1df59014997e1
lisa0104 <59335342+lisa0104@users.noreply.github.com>
2022-11-20 17:25:32 +0700

    commit perrtama

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+
 # BE-Group-Project-2
+
---
+++ routes/dampak.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { authJwt } = require("../middlewares");
+
+
+const controller = require("../controllers/dampakController")
+
+const {createDampak, updateDampak, deleteDampak, getSingleDampak, getAllDampaks} = require('../controllers/dampakController');
+
+router.route('/').post(createDampak);
+
+router.route('/:id').put(updateDampak);
+router.route('/:id').delete(deleteDampak);
+router.route('/:id').get(getSingleDampak);
+router.route('/').get(getAllDampaks);
+
+module.exports = router
+
+
+
+module.exports = function(app) {
+    app.use(function(req, res, next) {
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, Content-Type, Accept"
+      );
+      next();
+    });
+  
+    app.get("/userpage", [authJwt.verifyToken], controller.getInformasi)
+    app.post("/userpage", [authJwt.verifyToken], controller.addInformasi)
+
+    app.post('/', [authJwt.verifyToken], controller.createDampak)
+
+    app.put('/:id', [authJwt.verifyToken], controller.updateDampak)
+    app.delete('/:id'[authJwt.verifyToken], controller.deleteDampak)
+    app.get('/:id', [authJwt.verifyToken], controller.getSingleDampak)
+    app.get('/', [authJwt.verifyToken], controller.getAllDampaks)
+  
+    
+  };
