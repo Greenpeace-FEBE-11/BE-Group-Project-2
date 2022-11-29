3d6f39af661b9c8c88bfa85be98b8e6a647b5e65
febrianto <febriantomanangi@gmail.com>
2022-11-29 23:14:43 +0800

    febrianto - ubah config

--- config/db.config.js
+++ config/db.config.js
@@ -2,12 +2,20 @@ const mongoose = require('mongoose')
 
 const url = "mongodb://localhost:27017/voluntegreen"
 
-const db = mongoose.connect(url,  {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  
-})
+async function connect(){
+      try {
+          await mongoose.connect(url,  {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+          useFindAndModify: false,
+          
+        })
+      } catch (error) {
+          console.log(error);
+      }
+}
+
+const db = connect()
 
 module.exports = db
